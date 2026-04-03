025fb4278822b1c9668b90f24b05de19a68d8476
devi698 <devi69@zohomail.in>
2026-04-03 08:54:16 +0000

    fix: remove session check from symbols API and remove limit to show all master contract symbols
    - Remove @check_session_validity from /api/symbols to allow fetching symbols
    - Remove limit(1000) to return all available symbols from master contract

--- blueprints/strategy.py
+++ blueprints/strategy.py
@@ -687,11 +687,10 @@ def search_symbols():
 
 
 @strategy_bp.route("/api/symbols")
-@check_session_validity
 def api_get_symbols():
     """API: Get all available symbols"""
     from database.symbol import SymToken
-    symbols = SymToken.query.with_entities(SymToken.symbol).distinct().order_by(SymToken.symbol).limit(1000).all()
+    symbols = SymToken.query.with_entities(SymToken.symbol).distinct().order_by(SymToken.symbol).all()
     symbol_list = [s[0] for s in symbols]
     return jsonify({"symbols": symbol_list})
 
